247a746d70b782444905bf1ad3cc0ccf194ab908
Oskars Tuns <oskars@scandiweb.com>
2014-10-05 14:39:37 +0300

    final page update

--- application/views/success.php
+++ application/views/success.php
@@ -1,7 +1,7 @@
 <h3>Your Greeting is ready and will be available for 30 days!</h3>
 
 <ul style="padding:20px;">
-    <a href="http://igreetyou.com/index.php/show/index/<?php echo $subdomain; ?>">http://igreetyou.com/index.php/show/index/<?php echo $subdomain; ?></a>
+    <a href="http://igreetyou.com/index.php/show/index/<?php echo $subdomain; ?>">http://<?php echo $subdomain; ?>.igreetyou.com</a>
 </ul>
 
 <p><?php /*echo anchor('slides', 'Upload Another File!'); */?>
